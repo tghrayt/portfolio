3886a1eed2e8e0fe02b33df707ac40d90abc72a3
Dilanka Rathnasiri <rdilanka17@gmail.com>
2026-01-31 14:40:46 +0530

    install prime ng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { CustomPreset } from './custom-preset';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideHttpClient(),
+        providePrimeNG({
+            theme: {
+                preset: CustomPreset,
+                options: {
+                    darkModeSelector: false,
+                },
+            },
+            ripple: true,
+        }),
     ],
 };
