379b958b27909fe55ae389f217997eeacdb8edf8
Dilanka Rathnasiri <rdilanka17@gmail.com>
2026-02-01 07:48:42 +0530

    replace extend from bootstrap in styles.scss

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,6 +7,7 @@ import { NavBarItem } from '../dto/NavBarItem';
     selector: 'app-nav-bar',
     imports: [RouterLink, RouterLinkActive, SocialMediaComponent],
     templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
     menuItems: NavBarItem[] = [
