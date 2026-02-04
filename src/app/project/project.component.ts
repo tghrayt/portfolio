b7e254056049f756ad9dc3ccefe52ac8ed51d138
Dilanka Rathnasiri <rdilanka17@gmail.com>
2026-02-04 19:33:46 +0530

    replace bootstrap cards with prime-ng card

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 import { ProjectItem } from '../dto/ProjectItem';
 
 @Component({
     selector: 'app-project',
-    imports: [],
+    imports: [CardModule, ButtonModule],
     templateUrl: './project.component.html',
+    styleUrl: './project.component.scss',
 })
 export class ProjectComponent implements OnInit {
     projects: ProjectItem[] = [];
