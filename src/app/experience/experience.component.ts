4c051beb968032e491784fd9df2b7c7ff5c8c87f
Dilanka Rathnasiri <rdilanka17@gmail.com>
2026-02-21 18:31:35 +0530

    feat: replace bootstrap with tailwind in experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -8,7 +8,6 @@ import { ExperienceItem } from '../dto/ExperienceItem';
     selector: 'app-experience',
     imports: [CardModule],
     templateUrl: './experience.component.html',
-    styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent implements OnInit {
     experienceItems: ExperienceItem[] = [];
