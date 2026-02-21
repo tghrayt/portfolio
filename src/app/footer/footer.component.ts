a3512f02e09953e554602c6733cb054ec6d6d547
Dilanka Rathnasiri <rdilanka17@gmail.com>
2026-02-21 12:40:38 +0530

    feat: replace bootstrap with tailwind in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,7 +5,6 @@ import { SocialMediaComponent } from '../social-media/social-media.component';
     selector: 'app-footer',
     imports: [SocialMediaComponent],
     templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
     currentYear: number = new Date().getFullYear();
