96b29b4fa22817432e28b34fbb355aff99093ea8
Dilanka Rathnasiri <rdilanka17@gmail.com>
2026-02-28 23:27:39 +0530

    feat: update tech stack in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { SocialMediaComponent } from '../social-media/social-media.component';
 
 @Component({
     selector: 'app-footer',
-    imports: [SocialMediaComponent],
+    imports: [SocialMediaComponent, NgOptimizedImage],
     templateUrl: './footer.component.html',
 })
 export class FooterComponent {
